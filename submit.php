8843586aa57caa8cc8ea552cf4cb419a5e363bd0
abdraoufai <150270015+abdraoufai@users.noreply.github.com>
2024-06-05 22:21:55 +0100

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,9 @@
+<?php
+$instagram_url = "https://www.facebook.com/nike/";
+$facebook_url = "https://twitter.com/Nike";
+$twitter_url = "https://www.instagram.com/nike/?hl=en";
+?>
+<a href="<?php echo $instagram_url; ?>" target="_blank"><img src="instagram.png" alt="Instagram"></a>
+<a href="<?php echo $facebook_url; ?>" target="_blank"><img src="facebook.png" alt="Facebook"></a>
+<a href="<?php echo $twitter_url; ?>" target="_blank"><img src="twitter.png" alt="Twitter"></a>
+?>
